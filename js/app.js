3c2425ea06c70f47ab1d707ff1915e9d6f9ba2ba
m0-ar <s176492@student.dtu.dk>
2021-12-16 16:34:52 +0100

    Fix bugs

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
+  $(window).on('load', function() {
+    $('.preloader').addClass('complete');
+  });
 
-$(window).on('load',function(){
-  $('.preloader').addClass('complete')
-});
-
-  $(window).on('scroll', function(){
+  $(window).on('scroll', function() {
     var scroll = $(window).scrollTop();
     if (scroll >= 50) {
       $(".sticky").addClass("stickyadd");
@@ -15,7 +14,7 @@ $(window).on('load',function(){
   })
 
   var typed = new Typed(".element", {
-    strings: ["Mohamad Ashmar","a Software Engineer"],
+    strings: ["Mohamad Ashmar", "a Software Engineer"],
     smartBackspace: true,
     typeSpeed: 100,
     backSpeed: 100,
@@ -25,25 +24,25 @@ $(window).on('load',function(){
   });
 
 
-// Progress bars
-var waypoint = new Waypoint({
-  element: document.getElementById('exp-id'),
-  handler: function() {
+  // Progress bars
+  var waypoint = new Waypoint({
+    element: document.getElementById('exp-id'),
+    handler: function() {
 
-    console.log("waypoint")
+      console.log("waypoint")
 
-    var p = document.querySelectorAll('.progress-bar');
-    p[0].setAttribute("style", "width:75%;transition:1s all;");
-    p[1].setAttribute("style", "width:80%;transition:1.5s all;");
-    p[2].setAttribute("style", "width:60%;transition:1.7s all;");
-    p[3].setAttribute("style", "width:80%;transition:2s all;");
-    p[4].setAttribute("style", "width:70%;transition:2s all;");
-  },
-  offset: '90%'
-});
+      var p = document.querySelectorAll('.progress-bar');
+      p[0].setAttribute("style", "width:75%;transition:1s all;");
+      p[1].setAttribute("style", "width:80%;transition:1.5s all;");
+      p[2].setAttribute("style", "width:60%;transition:1.7s all;");
+      p[3].setAttribute("style", "width:80%;transition:2s all;");
+      p[4].setAttribute("style", "width:70%;transition:2s all;");
+    },
+    offset: '90%'
+  });
 
-// Owl Carousel, Todo: not working
-$('.owl-carousel').owlCarousel();
+  // Owl Carousel, Todo: not working
+  $('.owl-carousel').owlCarousel();
 
 });
 
