7307d5e68a3a877427e570906a36590710e6630a
m0-ar <s176492@student.dtu.dk>
2021-12-16 18:25:18 +0100

    Different changes

--- js/app.js
+++ js/app.js
@@ -32,9 +32,9 @@ $(document).ready(function() {
       console.log("waypoint")
 
       var p = document.querySelectorAll('.progress-bar');
-      p[0].setAttribute("style", "width:75%;transition:1s all;");
+      p[0].setAttribute("style", "width:80%;transition:1s all;");
       p[1].setAttribute("style", "width:80%;transition:1.5s all;");
-      p[2].setAttribute("style", "width:60%;transition:1.7s all;");
+      p[2].setAttribute("style", "width:70%;transition:1.7s all;");
       p[3].setAttribute("style", "width:80%;transition:2s all;");
       p[4].setAttribute("style", "width:70%;transition:2s all;");
     },
@@ -49,4 +49,8 @@ $(document).ready(function() {
 // Work
 var filterized = $(".filter-container").filterizr({
   animationDuration: .5,
+});
+
+$('a').smoothScroll({
+  speed:2000,
 });
